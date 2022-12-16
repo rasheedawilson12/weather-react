92a0dd6185e513bd6648443e07db27bdb174cf8f
rasheedawilson12 <rasheedawilson12@gmail.com>
2022-12-15 19:46:39 -0600

    react file tranfer

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+import Weather from "./Weather";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Weather city="Madrid" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
